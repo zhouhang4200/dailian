cfb250655c0c6907f8084201095bed47a861d885
zhouhang <zhouhang4200@163.com>
2018-08-10 16:56:14 +0800

    公告中心默认开启，帮助中心默认关闭

--- resources/views/front/help/index.blade.php
+++ resources/views/front/help/index.blade.php
@@ -20,8 +20,12 @@
                     @endforelse
                 </ul>
                 <div class="layui-tab-content">
-                    @forelse($categories as $category)
-                        <div class="layui-tab-item">
+                    @forelse($categories as $k => $category)
+                        @if($k == 0)
+                            <div class="layui-tab-item layui-show">
+                                @else
+                                    <div class="layui-tab-item">
+                                        @endif
                             <div class="layui-collapse" lay-filter="test">
                                 @forelse($category->articles as $article)
                                     @if($article->status == 1)

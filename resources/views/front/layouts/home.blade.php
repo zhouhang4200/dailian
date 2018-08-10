030adc1d163ed3c32e181d04898de61e645cffd7
Leal <442962403@qq.com>
2018-08-10 16:55:53 +0800

    调整底部版本样式

--- resources/views/front/layouts/home.blade.php
+++ resources/views/front/layouts/home.blade.php
@@ -13,7 +13,7 @@
     <title>丸子代练 @yield('title')</title>
 </head>
 
-<body>
+<body style="position: relative">
 <div class="header">
     <div class="logo">
         <img src="/front/images/logo.png" alt="">
@@ -46,7 +46,7 @@
     </div>
 </div>
 @yield('main')
-<div class="" style="height: 40px;line-height: 40px;width:100%;background-color:#2b2d36;color:#fff;position: relative;bottom: 0">
+<div class="" style="height: 40px;line-height: 40px;width:100%;background-color:#2b2d36;color:#fff;position: absolute;bottom: 0">
     <div style="width: 600px;text-align: center;margin: 0 auto;font-size: 12px">版权所有 2010-2018 武汉一游玩网络科技有限公司 鄂ICP备 15007777号-1 鄂网文 ( 2016 ) 1126-22 号 </div>
 </div>
 <script src="/front/lib/js/layui/layui.js"></script>

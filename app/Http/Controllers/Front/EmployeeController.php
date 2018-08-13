3645efbe10a32822e935501ed95ee050442b4d44
zhouhang <zhouhang4200@163.com>
2018-08-13 18:09:00 +0800

    添加员工的时候判断手机号格式

--- app/Http/Controllers/Front/EmployeeController.php
+++ app/Http/Controllers/Front/EmployeeController.php
@@ -67,9 +67,15 @@ class EmployeeController extends Controller
      */
     public function store(Request $request)
     {
+        // 判断手机号位数是否正确
+        $isRight = strlen($request->data['phone']);
+
+        if ($isRight != 11) {
+            return response()->json(['status' => 0, 'message' => '请输入正确的手机号']);
+        }
+
         // 判断手机号是否唯一
         $isSingle = User::where('phone', $request->data['phone'])->withTrashed()->first();
-
         if ($isSingle) {
             return response()->json(['status' => 0, 'message' => '账号已存在']);
         }

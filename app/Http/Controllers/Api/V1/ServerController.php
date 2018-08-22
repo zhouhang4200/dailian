c8d177acd7295fbc13e43c3fabcdd945fc4129a0
Leal <442962403@qq.com>
2018-08-22 18:29:48 +0800

    完成游戏 服 区 接口

--- app/Http/Controllers/Api/V1/ServerController.php
+++ app/Http/Controllers/Api/V1/ServerController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-use Illuminate\Http\Request;
+use App\Models\Server;
 use App\Http\Controllers\Controller;
 
+/**
+ * Class ServerController
+ * @package App\Http\Controllers\Api\V1
+ */
 class ServerController extends Controller
 {
-    //
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        if (! request('region_id')){
+            return response()->apiJson(1001);
+        }
+
+        return response()->apiJson(0, Server::select('region_id', 'id', 'name')
+            ->where('region_id', request('region_id'))->get());
+    }
 }

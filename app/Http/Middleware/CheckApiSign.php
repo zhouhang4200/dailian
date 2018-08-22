333290ea732983bfe276cca4349a0200b6031db6
Leal <442962403@qq.com>
2018-08-22 16:00:51 +0800

    完成待接单列表与详情

--- app/Http/Middleware/CheckApiSign.php
+++ app/Http/Middleware/CheckApiSign.php
@@ -15,34 +15,34 @@ class CheckApiSign
      */
     public function handle($request, Closure $next)
     {
-        $data = [];
+        // 在生产环境才做签名检测
+        if (app()->environment() == 'production') {
+            $data = [];
+            // 检测 sign
+            if (! isset($request->sign) || ! isset($request->timestamp)) {
+                $data = config('api.code')['1001'];
+                $data['data'] = [];
+                return response()->json($data);
+            }
 
-        // 检测 sign
-        if (! isset($request->sign) || ! isset($request->timestamp)) {
-            $data = config('api.code')['1001'];
-            $data['data'] = [];
-            return response()->json($data);
-        }
+            $par = $request->all();
 
-        $par = $request->all();
+            ksort($par);
+            $str = '';
+            foreach ($par as $key => $value) {
+                if (! in_array($key, ['sign', 'avatar', 'image', 'image_1', 'image_2', 'image_3'])) {
+                    $str .= $key . '=' . $value . '&';
+                }
+            }
 
-        ksort($par);
-        $str = '';
-        foreach ($par as $key => $value) {
-            if (! in_array($key, ['sign', 'avatar', 'image', 'image_1', 'image_2', 'image_3'])) {
-                $str .= $key . '=' . $value . '&';
+            $sign = md5(rtrim($str,  '&') . 'ajJKDej2jF');
+
+            if ($sign != $request->sign) {
+                $data = config('api.code')['1002'];
+                $data['data'] = [];
+                return response()->json($data);
             }
         }
-
-        $sign = md5(rtrim($str,  '&') . 'ajJKDej2jF');
-//dd($sign);
-//        myLog('sign', ['sign' => $sign, 'str' => $str]);
-        if ($sign != $request->sign) {
-            $data = config('api.code')['1002'];
-            $data['data'] = [];
-            return response()->json($data);
-        }
-
         return $next($request);
     }
 }

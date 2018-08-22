86492b308ce8d29bed8b9a1881e85d5d4dfe8424
Leal <442962403@qq.com>
2018-08-22 16:02:46 +0800

    修改签名检测为生产环境时才执行

--- app/Http/Middleware/CheckApiSign.php
+++ app/Http/Middleware/CheckApiSign.php
@@ -9,39 +9,39 @@ class CheckApiSign
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         // 在生产环境才做签名检测
         if (app()->environment() == 'production') {
+
             $data = [];
             // 检测 sign
-            // 检测 sign
-            if (! isset($request->sign) || ! isset($request->timestamp)) {
+            if (!isset($request->sign) || !isset($request->timestamp)) {
                 return response()->apiJson(1001);
             }
 
-        $par = $request->all();
+            $par = $request->all();
 
-        ksort($par);
-        $str = '';
-        foreach ($par as $key => $value) {
-            if (! in_array($key, ['sign', 'avatar', 'image', 'image_1', 'image_2', 'image_3'])) {
-                $str .= $key . '=' . $value . '&';
+            ksort($par);
+            $str = '';
+            foreach ($par as $key => $value) {
+                if (!in_array($key, ['sign', 'avatar', 'image', 'image_1', 'image_2', 'image_3'])) {
+                    $str .= $key . '=' . $value . '&';
+                }
             }
+
+            $sign = md5(rtrim($str, '&') . 'ajJKDej2jF');
+
+            if ($sign != $request->sign) {
+                return response()->apiJson(1002);
+
+            }
+
         }
-
-        $sign = md5(rtrim($str,  '&') . 'ajJKDej2jF');
-//dd($sign);
-//        myLog('sign', ['sign' => $sign, 'str' => $str]);
-        if ($sign != $request->sign) {
-            return response()->apiJson(1002);
-
-        }
-
         return $next($request);
     }
 }

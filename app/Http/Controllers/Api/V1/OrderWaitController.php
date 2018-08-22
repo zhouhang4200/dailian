4a646c5c601386560d9b3f4a245efbc91fff4a46
Leal <442962403@qq.com>
2018-08-22 14:08:26 +0800

    增加订单 游戏 区 服 控制器

---
+++ app/Http/Controllers/Api/V1/OrderWaitController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\GameLevelingOrder;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class OrderWaitController extends Controller
+{
+    public function index()
+    {
+        GameLevelingOrder::
+    }
+}

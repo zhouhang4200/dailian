23cf45afbc0e78f3b2bdff4329202ec6b3047630
Leal <442962403@qq.com>
2018-08-23 15:31:22 +0800

    订单列表图标增加网址

--- app/Http/Controllers/Api/V1/OrderOperationController.php
+++ app/Http/Controllers/Api/V1/OrderOperationController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Api\V1;
 
-
+use Exception;
 use App\Exceptions\Order\OrderException;
-use App\Exceptions\UserAsset\UserAssetBalanceException;
 use App\Exceptions\UserAsset\UserAssetException;
+use App\Exceptions\UserAsset\UserAssetBalanceException;
 use App\Http\Controllers\Controller;
 use App\Services\OrderService;
 
@@ -30,10 +30,12 @@ class OrderOperationController extends Controller
             OrderService::init(request()->user()->id, request('trade_no'))
                 ->take(request('pay_password'), request('take_password'));
         } catch (UserAssetBalanceException $exception) {
-
+            return response()->apiJson(7001);
         } catch (UserAssetException $exception) {
-            return response()->apiJson(1003);
+            return response()->apiJson(7001);
         } catch (OrderException $exception) {
+            return response()->apiJson(7001);
+        } catch (Exception $exception) {
             return response()->apiJson(1003);
         }
         return response()->apiJson(0);

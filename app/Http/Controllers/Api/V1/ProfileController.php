d70a59cfc991e8611d673937d6d651bd6bea5fb2
zhouhang <zhouhang4200@163.com>
2018-08-24 11:46:43 +0800

    url换为asset

--- app/Http/Controllers/Api/V1/ProfileController.php
+++ app/Http/Controllers/Api/V1/ProfileController.php
@@ -41,7 +41,7 @@ class ProfileController extends Controller
             $data['phone'] = $user->phone;
             $data['wechat'] = $user->wechat;
             $data['qq'] = $user->qq;
-            $data['avatar'] = url($user->avatar);
+            $data['avatar'] = asset($user->avatar);
             $data['status'] = $user->status;
             $data['certification_status'] = $user->realNameCertification ? $user->realNameCertification->status : 0;
             $data['balance'] = $user->userAsset ? $user->userAsset->balance : 0;

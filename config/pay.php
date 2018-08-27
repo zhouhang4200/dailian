0421f4a9d2ec1eb32f20dc008b2f4f20b2ea4173
zhouhang <zhouhang4200@163.com>
2018-08-27 14:18:36 +0800

    将miniapp_id写到appid那里

--- config/pay.php
+++ config/pay.php
@@ -27,9 +27,9 @@ return [
         // 小程序 APPID
         'miniapp_id' => env('WECHAT_MINIAPP_ID', 'wx2c4853cb397d357c'),
         // APP 引用的 appid
-        'appid' => env('WECHAT_APPID', ''),
+        'appid' => env('WECHAT_APPID', 'wx2c4853cb397d357c'),
         // 微信支付分配的微信商户号
-        'mch_id' => env('WECHAT_MCH_ID', ''), // 1502886431
+        'mch_id' => env('WECHAT_MCH_ID', '1502886431'), // 1502886431
         // 微信支付异步通知地址
         'notify_url' => '',
         // 微信支付签名秘钥

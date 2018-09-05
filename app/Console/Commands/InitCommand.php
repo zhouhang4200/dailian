cc99a373ddda31c4c9dfff0d81e9b109f60a9ff7
Leal <442962403@qq.com>
2018-09-05 14:03:39 +0800

    更新：初始化命令

--- app/Console/Commands/InitCommand.php
+++ app/Console/Commands/InitCommand.php
@@ -2,8 +2,10 @@
 
 namespace App\Console\Commands;
 
-use Illuminate\Support\Facades\DB;
+use App\Models\Role;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
 
 class InitCommand extends Command
 {
@@ -39,22 +41,24 @@ class InitCommand extends Command
      */
     public function createFrontDefaultPermissionRole()
     {
-        $data = [
-            ['module_name' => '账号管理', 'name' => 'employee', 'alias' => '员工管理'],
-            ['module_name' => '账号管理', 'name' => 'employee.group', 'alias' => '岗位管理'],
-            ['module_name' => '财务管理', 'name' => 'finance.asset-flow', 'alias' => '资金明细'],
-            ['module_name' => '财务管理', 'name' => 'finance.balance-withdraw', 'alias' => '提现记录'],
-            ['module_name' => '财务管理', 'name' => 'finance.finance-report-day', 'alias' => '资金日报'],
-            ['module_name' => '财务管理', 'name' => 'finance.balance-recharge.record', 'alias' => '充值记录'],
-            ['module_name' => '订单管理', 'name' => 'order.take-list', 'alias' => '接单列表']
-        ];
+        if (! Role::where('user_id', 0)->exists()) {
+            $data = [
+                ['module_name' => '账号管理', 'name' => 'employee', 'alias' => '员工管理'],
+                ['module_name' => '账号管理', 'name' => 'employee.group', 'alias' => '岗位管理'],
+                ['module_name' => '财务管理', 'name' => 'finance.asset-flow', 'alias' => '资金明细'],
+                ['module_name' => '财务管理', 'name' => 'finance.balance-withdraw', 'alias' => '提现记录'],
+                ['module_name' => '财务管理', 'name' => 'finance.finance-report-day', 'alias' => '资金日报'],
+                ['module_name' => '财务管理', 'name' => 'finance.balance-recharge.record', 'alias' => '充值记录'],
+                ['module_name' => '订单管理', 'name' => 'order.take-list', 'alias' => '接单列表']
+            ];
 
-        DB::table('permissions')->insert($data);
+            DB::table('permissions')->insert($data);
 
-        DB::table('roles')->insert([['user_id' => 0, 'name' => 'default', 'alias' => '默认权限']]);
+            DB::table('roles')->insert([['user_id' => 0, 'name' => 'default', 'alias' => '默认权限']]);
 
-        $role = \App\Models\Role::where('name', 'default')->where('user_id', 0)->first();
-        $permissionIds = \App\Models\Permission::pluck('id')->toArray();
-        $role->permissions()->sync($permissionIds);
+            $role = \App\Models\Role::where('name', 'default')->where('user_id', 0)->first();
+            $permissionIds = \App\Models\Permission::pluck('id')->toArray();
+            $role->permissions()->sync($permissionIds);
+        }
     }
 }

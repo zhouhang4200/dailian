f098629634df7ab873864f69e0853c098d51caa8
Leal <442962403@qq.com>
2018-09-05 19:12:03 +0800

    更新：取消仲裁状态

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -945,7 +945,7 @@ class OrderService
         try {
             // 更新撤销数据
             $complain = GameLevelingOrderComplain::where('game_leveling_order_trade_no', self::$order->trade_no)->first();
-            $complain->status = 2;
+            $complain->status = 3;
             $complain->save();
             // 删除上传的仲裁图片
             foreach ($complain->image as $item) {

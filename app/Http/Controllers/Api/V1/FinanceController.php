b9479010dc3d3906435543aac5eabcf6ecaa04a7
Leal <442962403@qq.com>
2018-09-06 17:15:41 +0800

    更新：提现信息

--- app/Http/Controllers/Api/V1/FinanceController.php
+++ app/Http/Controllers/Api/V1/FinanceController.php
@@ -248,8 +248,8 @@ class FinanceController extends Controller
             $data = [
                 'balance' => auth()->user()->userAsset->balance,
                 'rate' => 0.01,
-                'tips' => '充值说明',
-                'min_amount' => 0.01,
+                'tips' => '提现金额最低10元且必须为整数，提现将收取1%的手续费，提现金额将在3个工作日（节假日顺延）转账到实名认证填写的银行卡上',
+                'min_amount' => 10,
                 'max_amount' => 1000,
             ];
 

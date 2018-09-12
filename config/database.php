3ac9662ff62922dcb0810f280d0d2b93ef20e6bb
Leal <442962403@qq.com>
2018-09-12 12:05:46 +0800

    更新：database.php 配置文件将redis 的三个库存分别改为 6 7 8，因线上多个项目共用一个redis

--- config/database.php
+++ config/database.php
@@ -127,19 +127,19 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 0,
+            'database' => 6,
         ],
         'session' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 1,
+            'database' => 7,
         ],
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => 2,
+            'database' => 8,
         ],
     ],
 

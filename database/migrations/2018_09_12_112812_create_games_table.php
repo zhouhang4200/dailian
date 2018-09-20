758101ef22f19e337d7db75c5fe5c79ed141e0aa
zhouhang <zhouhang4200@163.com>
2018-09-20 15:17:57 +0800

    修改游戏字段

--- database/migrations/2018_09_12_112812_create_games_table.php
+++ database/migrations/2018_09_12_112812_create_games_table.php
@@ -16,12 +16,11 @@ class CreateGamesTable extends Migration {
 		{
 			$table->increments('id');
 			$table->string('name', 60)->unique()->comment('游戏名称');
-			$table->integer('status')->comment('类型');
 			$table->string('initials', 8)->comment('首字母拼音');
 			$table->boolean('game_type_id')->comment('游戏类型:手游 端游');
 			$table->boolean('game_class_id')->comment('游戏类别: 射击 策略');
 			$table->string('icon', 191)->comment('游戏图标');
-			$table->tinyIncrements('status')->default(1)->comment('是否显示 1 显示 2 隐藏');
+			$table->tinyInteger('status')->default(1)->comment('是否显示 1 显示 2 隐藏');
 			$table->timestamps();
 		});
 	}
